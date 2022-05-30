45b34c323cb26c199d277fbb9e58b9b953bf2261
Blugon <71944969+Blugon0921@users.noreply.github.com>
2022-05-30 20:03:24 +0900

    ✨ 1.19_v1.0.0

--- assets/js/downloadpack.js
+++ assets/js/downloadpack.js
@@ -1,5 +1,5 @@
-let LastMcVer = "1.18"
-let LastPackVer = "1.0.6"
+let LastMcVer = "1.19"
+let LastPackVer = "1.0.0"
 
 function downloadpack(mcver, ver) {
     let pack = `pack/${mcver}/Improvement_${mcver}_v${ver}.zip`
